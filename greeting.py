fa858b5171867c0d126c845082595d0c58c79365
subramanian_anand <subramanian_anand@localhost>
2010-03-16 16:19:47 +0000

    Added logout, and moved auth\__init__.py into login.py

--- greeting.py
+++ greeting.py
@@ -2,36 +2,32 @@ import site
 site.addsitedir('/home/sanand/lib/python2.4/site-packages')
 site.addsitedir('/usr/local/lib/python2.4/site-packages')   # for mysql-python
 
-import web, jinja2, os.path, cgi, xml.dom.minidom, smsgateway
+import web, jinja2, os.path, cgi, xml.dom.minidom, smsgateway, login
 from model_mysql import *
-from auth import GoogleLogin, TwitterLogin, FacebookLogin
-from web import form
 from urllib import urlopen, urlencode
 try: import simplejson as json
 except ImportError: from django.utils import simplejson as json
 
-greetingform = form.Form(   # id, user, time,
-    form.Textbox('mobile'),
-    form.Textbox('name'),
-    form.Dropdown('event', ['Birthday', 'Anniversary']),
-    form.Textbox('date'),
-    form.Dropdown('relation', ['Family', 'Friend']),
-    form.Textarea('message'),
+greetingform = web.form.Form(   # id, user, time,
+    web.form.Textbox('mobile'),
+    web.form.Textbox('name'),
+    web.form.Dropdown('event', ['Birthday', 'Anniversary']),
+    web.form.Textbox('date'),
+    web.form.Dropdown('relation', ['Family', 'Friend']),
+    web.form.Textarea('message'),
 )
 
 env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.join(os.path.split(__file__)[0], 'template')))
 
 urls = (
   '/',                  'index',
-  '/login/google',      'GoogleLogin',
-  '/login/twitter',     'TwitterLogin',
-  '/login/facebook/?',  'FacebookLogin',
+  '/login/google',      'login.Google',
+  '/login/twitter',     'login.Twitter',
+  '/login/facebook/?',  'login.Facebook',
+  '/logout',            'login.Logout',
   '/sms',               'sms',
   '/reminder',          'reminder'
 )
-
-# Facebook {'username': u'root.node', 'first_name': u'Anand', 'last_name': u'Subramanian', 'name': u'Anand Subramanian', 'locale': u'en_US', 'session_expires': 1268740800, 'pic_square': u'http://profile.ak.fbcdn.net/v225/1772/65/q655833454_7617.jpg', 'session_key': u'3.1eXoKggHvlfOw4KzSanrNg__.3600.1268740800-655833454', 'profile_url': u'http://www.facebook.com/root.node', 'uid': 655833454}
-# Google {'locale': u'en', 'first_name': u'Anand', 'last_name': u'S', 'name': u'Anand S', 'email': u'root.node@gmail.com'}
 
 class greetingapp(web.application):
     '''Patch web.application to remove .wsgi or .fcgi script name'''
@@ -41,18 +37,14 @@ class greetingapp(web.application):
             web.ctx.homepath = os.path.split(web.ctx.homepath)[0]
             web.ctx.home = web.ctx.homedomain + web.ctx.homepath
 
-app = greetingapp(urls, globals(), autoreload=False)
-session = web.session.Session(app, store, initializer={})
-
 class index:
     def GET(self):
         web.header('Content-type', 'text/html')
         return env.get_template('index.html').render(
             home=web.ctx.home,
-            debug='Home: ' + repr(web.ctx.home),
             form=greetingform(),
-            greetings=session.has_key('user') and db.select('greeting', where='user = %d' % session.user) or None,
-            session=session
+            greetings=web.config._session.has_key('user') and db.select('greeting', where='user = "%s"' % web.config._session.user) or None,
+            session=web.config._session
         ).encode('utf-8')
 
 class sms:
@@ -68,17 +60,15 @@ class sms:
 class reminder:
     def POST(self):
         f = greetingform()
-        if 1 or session.has_key('user') and f.validates():
-            # fields = dict(f.d)
-            # fields['user'] = session.user
-            # db.insert('greeting', **fields)
+        if web.config._session.has_key('user') and f.validates():
+            fields = dict(f.d)
+            fields['user'] = web.config._session.user
+            db.insert('greeting', **fields)
             raise web.seeother('/')
         else:
             return 'You need to log in'
 
-class logout:
-    def GET(self):
-        del session['user']
-        web.seeother('/')
-
+app = greetingapp(urls, globals(), autoreload=True)
+session = web.session.Session(app, store, initializer={})
+web.config._session = session
 application = app.wsgifunc()
--- login.py
+++ login.py
@@ -1,42 +1,66 @@
 import web
-from urllib import urlopen, urlencode
-from xml.dom.minidom import parse
+from auth import webpyauth, auth # WebappAuth, RequestRedirect, HttpException
 
-try: import simplejson as json
-except ImportError:
-    from django.utils import simplejson as json
+class Logout:
+    def GET(self):
+        if web.config._session.has_key('user'): del web.config._session.user
+        if web.config._session.has_key('name'): del web.config._session.name
+        raise web.seeother('/')
 
-class login:
-    def GET(self, method):
-        if method == 'google':
-            # http://code.google.com/apis/accounts/docs/OpenID.html
-            endpoint_dom = parse(urlopen('https://www.google.com/accounts/o8/id'))
-            endpoint = endpoint_dom.getElementsByTagName('URI')[0].firstChild.wholeText
+class Login:
+    def GET(self):
+        try:
+            if self.has_user():
+                self.get_authenticated_user(self._on_auth)
+                raise webpyauth.RequestRedirect(url=web.ctx.home)
+            else:
+                self.authenticate_redirect()
+        except webpyauth.RequestRedirect, e:
+            raise web.seeother(e.url)
 
-            raise web.seeother(endpoint + '?' + urlencode({
-                'openid.return_to'          : 'http://localhost:8080/login/google/return'           ,    # TODO: Detect this? Configure this?
-                'openid.mode'               : 'checkid_setup'                                       ,
-                'openid.ns'                 : 'http://specs.openid.net/auth/2.0'                    ,
-                'openid.claimed_id'         : 'http://specs.openid.net/auth/2.0/identifier_select'  ,
-                'openid.identity'           : 'http://specs.openid.net/auth/2.0/identifier_select'  ,
-                'openid.ns.ax'              : 'http://openid.net/srv/ax/1.0'                        ,
-                'openid.ax.mode'            : 'fetch_request'                                       ,
-                'openid.ax.required'        : 'country,email,firstname,lastname'                    ,
-                'openid.ax.type.country'    : 'http://axschema.org/contact/country/home'            ,
-                'openid.ax.type.email'      : 'http://schema.openid.net/contact/email'              ,
-                'openid.ax.type.firstname'  : 'http://axschema.org/namePerson/first'                ,
-                'openid.ax.type.lastname'   : 'http://axschema.org/namePerson/last'                 ,
-            }))
-
-        elif method == 'google/return':
-            return repr(web.ctx.query)
-
+    def _on_auth(self, user):
+        if user:
+            # Store this information and assign a user ID
+            key, name, attr = self.get_user(user)
+            web.config._session.user = key
+            web.config._session.name = name
         else:
-            return 'Not supported'
+            web.debug('Did not get user')
+            # TODO: Send to failed auth page or something.
 
 
-'''
-C. OAuth
-    1. Get Google Contacts openid.ext2.request_token
-'''
+class Google(webpyauth.WebappAuth, auth.GoogleMixin, Login):
+    def __init__(self):
+        webpyauth.WebappAuth.__init__(self)
 
+    def has_user(self): return web.input().get("openid.mode", None)
+
+    # Google user = {'locale': u'en', 'first_name': u'Anand', 'last_name': u'S', 'name': u'Anand S', 'email': u'root.node@gmail.com'}
+    def get_user(self, user): return 'gg:' + user.get('email'), user.get('name', '') or user.get('email'), user
+
+class Twitter(webpyauth.WebappAuth, auth.TwitterMixin, Login):
+    def __init__(self):
+        # Register apps at http://twitter.com/apps/new
+        # http://twitter.com/oauth_clients/details/97296
+        webpyauth.WebappAuth.__init__(self,
+            twitter_consumer_key    = 'GuXOFFfhnlxs1kgMpsQhw',
+            twitter_consumer_secret = 'VL28QvbgbqW8cAssCUzqrtaXK3MxNM0pmXmeuQHYFQ',
+        )
+
+    def has_user(self): return web.input().get("oauth_token", None)
+    
+    def get_user(self, user): return 'tw:', '', {}
+
+class Facebook(webpyauth.WebappAuth, auth.FacebookMixin, Login):
+    def __init__(self):
+        # http://www.facebook.com/developers/editapp.php?app_id=369944562357
+        # Set Connect URL to the return value
+        webpyauth.WebappAuth.__init__(self,
+            facebook_api_key    = 'b7166989bdf0ad62b809237091d5d576',
+            facebook_secret     = '2b1b0705b4e1bea41f70402ea49799d7',
+        )
+
+    def has_user(self): return web.input().get("session", None)
+
+    # Facebook user = {'username': u'root.node', 'first_name': u'Anand', 'last_name': u'Subramanian', 'name': u'Anand Subramanian', 'locale': u'en_US', 'session_expires': 1268740800, 'pic_square': u'http://profile.ak.fbcdn.net/v225/1772/65/q655833454_7617.jpg', 'session_key': u'3.1eXoKggHvlfOw4KzSanrNg__.3600.1268740800-655833454', 'profile_url': u'http://www.facebook.com/root.node', 'uid': 655833454}
+    def get_user(self, user): return 'fb:' + str(user.get('uid', None)), user.get('name', ''), user
